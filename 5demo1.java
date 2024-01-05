9350abab3eb2d1ab45990f75c35120489ad57fee
22hr1a0573 <154883322+22hr1a0573@users.noreply.github.com>
2024-01-05 12:43:26 +0530

    Add files via upload

---
+++ 5demo1.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+class Test{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int dn;
+dn=sc.nextInt();
+
+if(dn==0){System.out.println("jan");}
+else if(dn==1){System.out.println("feb");}
+else if(dn==2){System.out.println("mar");}
+else if(dn==3){System.out.println("april");}
+else if(dn==4){System.out.println("may");}
+else if(dn==5){System.out.println("june");}
+else if(dn==6){System.out.println("july");}
+else if(dn==7){System.out.println("aug");}
+else if(dn==8){System.out.println("Sep");}
+else if(dn==9){System.out.println("oct");}
+else if(dn==10){System.out.println("nov");}
+else if(dn==11){System.out.println("dec");}
+
+else{System.out.println("INVALID");}
+}
+}
+
--- Suchi.java
+++ Suchi.java
@@ -1,17 +1,11 @@
-public class Suchi{
+import java.io.*;
+class Suchi{
 public static void main(String args[])
 {
-boolean a=true;
-boolean b=false;
 
-
-System.out.println("a:"+a);
-
-System.out.println("a:"+a);
-System.out.println("b:"+b);
-System.out.println("a&&b:"+(a&&b));
-System.out.println("a||b:"+(a||b));
-System.out.println("!a:"+(!a));
-System.out.println("!b:"+(!b));
+for(int i=1;i<=10;++i)
+  {
+   System.out.println("hello"+i);
+  }
 }
 }
